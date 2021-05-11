e0f1a95379c3556513a203ada5201112f41ad019
TonySilva7 <tony.deve7oper@gmail.com>
2021-05-11 10:05:38 -0300

    Refatorado e testado

--- app/src/main/java/com/tony/calculadora/MainActivity.java
+++ app/src/main/java/com/tony/calculadora/MainActivity.java
@@ -2,152 +2,192 @@ package com.tony.calculadora;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    private String operacao = "";
+    private String operator = "";
     private String n1 = "";
 
     private Integer num1 = 0;
     private Integer num2 = 0;
-    private EditText etScreen;
-    private TextView tvDisplay;
+    private EditText secondaryDisplay;
+    private TextView mainDisplay;
 
-
-    @Override
-    public View findViewById(int id) {
-        return super.findViewById(id);
-    }
+    private static final String TAG = "myDebug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        etScreen = (EditText) findViewById(R.id.etScreen);
-        tvDisplay = (TextView) findViewById(R.id.tvDisplay);
+
+        // instancia elementos da view
+        mainDisplay = (TextView) findViewById(R.id.mainDisplay);
+        secondaryDisplay = (EditText) findViewById(R.id.secondaryDisplay);
     }
 
-    public void pegaBotao(View v){
+    // método para capturar conteúdo dos botões numéricos e escreve-lo no screen
+    public void getButton(View v) {
+        // captura conteúdo do botão
         n1 = (String) ((Button) v).getText();
-        etScreen.append(n1);
-        tvDisplay.append(n1);
-    };
+        Log.d(TAG, "(String) ((Button) v).getText() = " + n1);
+        // escreve na tela dados recebidos
+        secondaryDisplay.append(n1);
+        mainDisplay.append(n1);
 
-    public Integer numero(){
-        if ((etScreen.getText().length() > 0) || !etScreen.getText().toString().contains("")) {
-            return Integer.parseInt(String.valueOf(etScreen.getText()));
-        }
-        else {
+    }
+
+    ;
+
+    // testa se o conteúdo da tela não está vazio e é maior que zero, então converte-o para inteiro.
+    public Integer toNumber() {
+        if ((secondaryDisplay.getText().length() > 0) || !secondaryDisplay.getText().toString().contains("")) {
+            int num = Integer.parseInt(String.valueOf(secondaryDisplay.getText()));
+            Log.d(TAG, "numero() retornou " + num);
+            return num;
+        } else {
             return 0;
         }
-    };
-
-    public Integer soma(Integer a, Integer b){ return (a+b);}
-    public Integer sub(Integer a, Integer b){
-        return (a-b);
     }
-    public Integer divide(Integer a, Integer b){
+
+    ;
+
+    // Método que calcula a soma
+    public Integer sum(Integer a, Integer b) {
+        Log.d(TAG, "Soma = " + (a + b));
+        return (a + b);
+    }
+
+    // Método para calcular a subtração
+    public Integer subtraction(Integer a, Integer b) {
+        return (a - b);
+    }
+
+    // Método para calcular a divisão
+    public Integer division(Integer a, Integer b) {
         return (a / b);
     }
-    public Integer multiplica(Integer a, Integer b){
+
+    // Método que calcula multiplicação
+    public Integer multiply(Integer a, Integer b) {
         return (a * b);
     }
 
-    public void fazOperacao(View v) {
-
-        if (((String) ((Button) v).getText().toString()).equals("CE") ){
+    // Método para armazenar os botões do tipo operadores
+    public void getOperation(View v) {
+        // Se o conteúdo obtido for "CE", Reseta os dados da tela
+        if (((String) ((Button) v).getText().toString()).equals("CE")) {
             num1 = 0;
             num2 = 0;
-            etScreen.setText("");
-            tvDisplay.setText("");
+            secondaryDisplay.setText("");
+            mainDisplay.setText("");
         } else {
-            tvDisplay.append((String) ((Button) v).getText().toString());
+            // Escreve na tela o conteúdo capturado
+            mainDisplay.append((String) ((Button) v).getText().toString());
+            Log.d(TAG, "Display Principal = " + ((String) ((Button) v).getText().toString()));
+
+            // Se num1 ainda não estiver setado, receberá o primeiro operando
             if (num1 == 0) {
-                num1 = numero();
-                etScreen.setText("");
-                operacao = (String) ((Button) v).getText().toString();
+                num1 = toNumber();
+                // limpa o display secundário
+                secondaryDisplay.setText("");
+                // Captura o operador da operação
+                operator = (String) ((Button) v).getText().toString();
             } else {
-                switch (operacao) {
+                /*
+                 * O Switch abaixo é para operações com mais de dois operandos.
+                 * Pra todos os casos abaixo, se num1 já tiver um valor, ele será resolvido.
+                 */
+                switch (operator) {
+                    //
                     case "+":
                         if (num1 == 0) {
-                            num1 = numero();
+                            num1 = toNumber();
                         } else {
-                            num1 = soma(num1, numero());
+                            num1 = sum(num1, toNumber());
                         }
-                        etScreen.setText("");
-                        operacao = (String) ((Button) v).getText().toString();
+                        secondaryDisplay.setText("");
+                        operator = (String) ((Button) v).getText().toString();
                         break;
                     case "-":
                         if (num1 == 0) {
-                            num1 = numero();
+                            num1 = toNumber();
                         } else {
-                            num1 = sub(num1, numero());
+                            num1 = subtraction(num1, toNumber());
                         }
-                        etScreen.setText("");
-                        operacao = (String) ((Button) v).getText().toString();
+                        secondaryDisplay.setText("");
+                        operator = (String) ((Button) v).getText().toString();
                         break;
                     case "/":
                         if (num1 == 0) {
-                            num1 = numero();
+                            num1 = toNumber();
                         } else {
-                            num1 = divide(num1, numero());
+                            num1 = division(num1, toNumber());
                         }
-                        etScreen.setText("");
-                        operacao = (String) ((Button) v).getText().toString();
+                        secondaryDisplay.setText("");
+                        operator = (String) ((Button) v).getText().toString();
                         break;
                     case "X":
                         if (num1 == 0) {
-                            num1 = numero();
+                            num1 = toNumber();
                         } else {
-                            num1 = multiplica(num1, numero());
+                            num1 = multiply(num1, toNumber());
                         }
-                        etScreen.setText("");
-                        operacao = (String) ((Button) v).getText().toString();
+                        secondaryDisplay.setText("");
+                        operator = (String) ((Button) v).getText().toString();
                         break;
                 }
             }
         }
     }
 
-    public void fazBinario(View v){
-        num1 = numero();
+    // Método para obter o correspondente binário do conteúdo digitado.
+    public void fazBinario(View v) {
+        num1 = toNumber();
         StringBuffer resultado = new StringBuffer();
         int sobra = 0;
         do {
-            sobra = num1%2;
-            num1 = num1/2;
+            sobra = num1 % 2;
+            num1 = num1 / 2;
             resultado.append(sobra);
-        }while (num1 != 0);
-        etScreen.setText(resultado.reverse());
+        } while (num1 != 0);
+
+        secondaryDisplay.setText(resultado.reverse());
     }
 
-    public void resultado(View v){
-        num2 = numero();
-        tvDisplay.append("=");
-        etScreen.setText("");
-        switch(operacao){
+
+    // Método para imprimir o resultado das operações no screen da calculadora
+    public void resultado(View v) {
+
+        num2 = toNumber(); // Recebe o segundo operando
+        Log.d(TAG, "Num2 = " + num2);
+        mainDisplay.append("="); // imprime sinal de "igual"
+        secondaryDisplay.setText(""); // limpa display secundário
+
+        // Switch para efetivar o a operação baseado na escolha da operação desejada
+        switch (operator) {
             case "+":
-                tvDisplay.append(soma(num1,num2).toString());
-                num1 = soma(num1,num2);
+                mainDisplay.append(sum(num1, num2).toString());
+                Log.d(TAG, "Case soma (+) = " + sum(num1, num2).toString());
+                num1 = sum(num1, num2);
                 break;
+
             case "-":
-
-                tvDisplay.append(sub(num1, num2).toString());
-                num1 = sub(num1,num2);
+                mainDisplay.append(subtraction(num1, num2).toString());
+                num1 = subtraction(num1, num2);
                 break;
+
             case "x":
-
-                tvDisplay.append(multiplica(num1, num2).toString());
-                num1 = multiplica(num1,num2);
+                mainDisplay.append(multiply(num1, num2).toString());
+                num1 = multiply(num1, num2);
                 break;
-            case "÷":
 
-                tvDisplay.append(divide(num1, num2).toString());
-                num1 = divide(num1,num2);
+            case "÷":
+                mainDisplay.append(division(num1, num2).toString());
+                num1 = division(num1, num2);
                 break;
         }
     }
